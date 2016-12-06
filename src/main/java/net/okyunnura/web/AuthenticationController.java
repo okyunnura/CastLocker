a496d37b25017890711e01b81fb32c55661c2047
okumura <okumura@tagbangers.co.jp>
2016-12-07 08:40:06 +0900

    scaffolding

---
+++ src/main/java/net/okyunnura/web/AuthenticationController.java
@@ -0,0 +1,29 @@
+package net.okyunnura.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AuthenticationController {
+
+	@RequestMapping(path = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping(path = "/login", method = RequestMethod.POST)
+	public String loginRedirect() {
+		return "redirect:/startup";
+	}
+
+	@RequestMapping(path = "/logout", method = RequestMethod.GET)
+	public String logout() {
+		return "logout";
+	}
+
+	@RequestMapping(path = "/login", method = RequestMethod.POST)
+	public String logoutRedirect() {
+		return "redirect:/startup";
+	}
+}

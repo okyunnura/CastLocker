19d17ad278b11e84203994000878d93b2eb49eff
okumura <okumura@tagbangers.co.jp>
2016-12-08 08:27:57 +0900

    scaffolding

---
+++ src/main/java/net/okyunnura/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package net.okyunnura.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		super.configure(web);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+	}
+}

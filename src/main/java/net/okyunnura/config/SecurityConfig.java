9f729fa39af50b600fbca3f71fb71e0633805fc2
okumura <ken@tagbangers.co.jp>
2016-12-18 23:28:13 +0900

    scaffolding

--- src/main/java/net/okyunnura/config/SecurityConfig.java
+++ src/main/java/net/okyunnura/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
@@ -15,14 +16,21 @@ import org.springframework.security.crypto.password.StandardPasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-				.withUser("user").password("password").roles("USER");
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+				.antMatchers("/css/**", "/js/**", "/img/**");
 	}
+
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication()
+//				.withUser("user").password("password").roles("USER");
+//	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+				.antMatchers("/startup").permitAll()
 				.anyRequest()
 				.authenticated();
 		http.formLogin()
@@ -37,7 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Configuration
-	public class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter{
+	public class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
 		private final AuthorizedService authorizedService;
 
 		@Autowired

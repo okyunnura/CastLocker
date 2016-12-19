b3ddec771a30fbff7240f0b201673466cbb300aa
okumura <ken@tagbangers.co.jp>
2016-12-20 08:38:00 +0900

    scaffolding

--- src/main/java/net/okyunnura/config/SecurityConfig.java
+++ src/main/java/net/okyunnura/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/startup").permitAll()
+				.antMatchers("/", "/startup").permitAll()
 				.anyRequest()
 				.authenticated();
 		http.formLogin()

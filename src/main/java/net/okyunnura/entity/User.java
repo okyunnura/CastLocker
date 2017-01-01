efc3579cd91d8317fd14e26002ab04a37f538a6b
okumura <ken@tagbangers.co.jp>
2017-01-02 08:20:47 +0900

    scaffolding

--- src/main/java/net/okyunnura/entity/User.java
+++ src/main/java/net/okyunnura/entity/User.java
@@ -7,8 +7,16 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.*;
-import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
